e1da29a5975b7cdf53567f2205c64d1980b20754
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-03-01 12:10:31 +0100

    ract on location change

--- app/src/main/java/funsol/io/dotty/MainActivity.java
+++ app/src/main/java/funsol/io/dotty/MainActivity.java
@@ -4,8 +4,6 @@ import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,7 +19,7 @@ import com.google.android.gms.maps.model.LatLng;
 public class MainActivity extends ActionBarActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 20000;
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = BuildConfig.DEBUG ? 1000 : 20000;
     public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
             UPDATE_INTERVAL_IN_MILLISECONDS / 2;
 
@@ -37,18 +35,21 @@ public class MainActivity extends ActionBarActivity implements
         setContentView(R.layout.activity_main);
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
 
         setup();
     }
 
     private void setup() {
         createLocationRequest();
-        centerAtMe(new LatLng(21.0000, 78.0000));
+        createApiClient();
+    }
+
+    private void createApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     private void createLocationRequest() {
@@ -58,14 +59,21 @@ public class MainActivity extends ActionBarActivity implements
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    private void centerAtMe(LatLng myPosition) {
-        map.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
-        map.animateCamera(CameraUpdateFactory.zoomTo(4));
+
+    private boolean centerAtMe(Location location) {
+        if(location != null) {
+            LatLng locationLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            map.moveCamera(CameraUpdateFactory.newLatLng(locationLatLng));
+            map.animateCamera(CameraUpdateFactory.zoomTo(14));
+            return true;
+        }
+        return false;
     }
 
     protected void startLocationUpdates() {
         LocationServices.FusedLocationApi.requestLocationUpdates(
                 mGoogleApiClient, mLocationRequest, this);
+
     }
 
     @Override
@@ -85,15 +93,17 @@ public class MainActivity extends ActionBarActivity implements
 
     @Override
     public void onConnected(Bundle bundle) {
-        Log.d(TAG, "Connected");
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        Log.d(TAG, "Connected " + mLastLocation);
+        centerAtMe(mLastLocation);
         startLocationUpdates();
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d(TAG, "Location chnaged to " + location);
-        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
-        centerAtMe(position);
+        Log.d(TAG, "Location changed to " + location);
+        centerAtMe(location);
     }
 
     @Override
